a404e930020443295594d1fde2a95ab8574b800d
sanghavig04-dot <sanghavig04@gmail.com>
2026-05-01 17:45:25 +0530

    Update api.js

--- api.js
+++ api.js
@@ -2,7 +2,7 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello from DevOps App 🚀");
+  res.end("Hello from DevOps API 🚀");
 });
 
 server.listen(3000, () => {
